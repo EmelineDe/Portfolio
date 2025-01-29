d0ca388a2dba77c20ca993bc7a73b46aeefb3f18
EmelineDe <delobel.emeline@gmail.com>
2025-01-29 20:12:16 +0100

    final colors choise

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,12 +2,12 @@ function Footer() {
   return (
     <div className="flex flex-col p-0 md:p-4 lg:p-4  md:flex-row items-center justify-center">
       <p className="text-sm">
-        © Developer Portfolio by{" "}
+        © Developer Portfolio by{"  "}
         <a
           href="https://linkedin.com/in/emeline-d-6915a417a"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-[#42bcbc] hover:text-[#ec704c] lg:text-[#42bcbc] lg:hover:text-[#ec704c]  dark:hover:text-[#42bcbc] lg:text-[#ec704c] lg:hover:text-[#42bcbc]"
+          className="text-[#ec704c] hover:text-[#42bcbc] dark:text-[#42bcbc] dark:hover:text-[#ec704c] lg:text-[#ec704c] lg:hover:text-[#42bcbc]"
         >
           Emeline Delobel - 2025
         </a>
